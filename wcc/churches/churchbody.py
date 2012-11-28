67ab2be02ee03a01c5364580f66a036a444420cf
Izhar Firdaus <izhar@inigo-tech.com>
2012-11-28 22:54:40 +0800

    move churchbody content from wcc.churchbody (re #88)

--- wcc/churches/churchbody.py
+++ wcc/churches/churchbody.py
@@ -5,7 +5,7 @@ from zope import schema
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from zope.interface import invariant, Invalid
+from zope.interface import invariant, Invalid, Interface
 
 from z3c.form import group, field
 
@@ -19,11 +19,13 @@ from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from wcc.churches import MessageFactory as _
-
+from plone.app.dexterity.behaviors.metadata import IBasic
+from wcc.churches.backref import back_references
 
 # Interface class; used to define content-type schema.
+from collective import dexteritytextindexer
 
-class IChurchBody(form.Schema, IImageScaleTraversable):
+class IChurchBody(form.Schema, IBasic, IImageScaleTraversable):
     """
     Church Body
     """
@@ -34,7 +36,98 @@ class IChurchBody(form.Schema, IImageScaleTraversable):
     # models/churchbody.xml to define the content type
     # and add directives here as necessary.
 
-    form.model("models/churchbody.xml")
+    title = schema.TextLine(
+        title = _(u'label_short_title', default=u'Short Name / Abbreviation'),
+        required = True
+    )
+
+    full_title = schema.TextLine(
+        title=_(u'label_full_title', u'Full Name'),
+        required=True
+    )
+
+    dexteritytextindexer.searchable('text')
+    form.widget(text="plone.app.z3cform.wysiwyg.WysiwygFieldWidget")
+    text = schema.Text(
+        title=_(u'label_body_text', u"Body Text"),
+        description=u'',
+        required=False,
+    )
+
+
+    member_of = RelationChoice(
+            title=_(u'label_member_of', u"Member Of"),
+            source=ObjPathSourceBinder(object_provides='wcc.churches.churchfamily.IChurchFamily'),
+            required=False
+    )
+
+    assoc_member_of = RelationChoice(
+            title=_(u'label_assoc_member_of', u'Associate Member Of'),
+            source=ObjPathSourceBinder(object_provides='wcc.churches.churchfamily.IChurchFamily'),
+            required=False
+    )
+
+    form.widget(other_members="plone.app.z3cform.wysiwyg.WysiwygFieldWidget")
+    other_members = schema.Text(
+        title=_(u'label_other_members', u'Other Members'),
+        required=False
+    )
+
+    form.widget(other_assoc_members="plone.app.z3cform.wysiwyg.WysiwygFieldWidget")
+    other_assoc_members = schema.Text(
+        title=_(u'label_other_assoc_members', u'Other Associate Members'),
+        required=False
+    )
+
+    form.order_before(full_title = '*')
+    form.order_before(title = '*')
+
+
+class IChurchBodyDataProvider(Interface):
+    pass
+
+class ChurchBodyDataProvider(grok.Adapter):
+    grok.context(IChurchBody)
+    grok.implements(IChurchBodyDataProvider)
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def title(self):
+        return self.context.title
+
+    @property
+    def text(self):
+        return self.context.text
+
+    @property
+    def member_of(self):
+        if self.context.member_of:
+            return self.context.member_of.to_object
+        return None
+
+    @property
+    def assoc_member_of(self):
+        if self.context.assoc_member_of:
+            return self.context.assoc_member_of.to_object
+        return None
+
+    @property
+    def members(self):
+        return back_references(self.context, 'member_of')
+
+    @property
+    def assoc_members(self):
+        return back_references(self.context, 'assoc_member_of')
+
+    @property
+    def other_members(self):
+        return self.context.other_members
+
+    @property
+    def other_assoc_members(self):
+        return self.context.other_assoc_members
 
 
 # View class
@@ -46,3 +139,6 @@ class Index(dexterity.DisplayForm):
     grok.context(IChurchBody)
     grok.require('zope2.View')
     grok.name('view')
+
+    def provider(self):
+        return IChurchBodyDataProvider(self.context)
