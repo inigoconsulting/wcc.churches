3bd1b77337f22d16cd0409c42076e81f9ae303a2
Izhar Firdaus <izhar@inigo-tech.com>
2013-03-07 19:32:24 +0800

    added upgrade step to rebuild intid (re #213)

--- wcc/churches/upgrades/handlers.py
+++ wcc/churches/upgrades/handlers.py
@@ -1,4 +1,24 @@
 from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
-
+from zope.intid.interfaces import IIntIds
+from zope.component import getUtility
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.churches to 1002',
+                description=u'Upgrade wcc.churches to 1002',
+                source='1', destination='1002',
+                sortkey=1, profile='wcc.churches:default')
+def to1002(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.churches.upgrades:to1002')
+    catalog = getToolByName(context, 'portal_catalog')
+    catalog.clearFindAndRebuild()
+    intids = getUtility(IIntIds)
+
+    for brain in catalog(Language='all'):
+        try:
+            obj = brain.getObject()
+            intids.register(obj)
+        except (AttributeError, KeyError):
+            pass
