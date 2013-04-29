5517e5c3673bf3525026df68dc1611ff7ec9cd49
Izhar Firdaus <izhar@inigo-tech.com>
2013-04-29 11:05:39 +0800

    fix broken backreference for churches

--- wcc/churches/upgrades/handlers.py
+++ wcc/churches/upgrades/handlers.py
@@ -3,7 +3,40 @@ from Products.CMFCore.utils import getToolByName
 from zope.intid.interfaces import IIntIds
 from zope.component import getUtility
 from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
+from zc.relation.interfaces import ICatalog
+from zope.component import getUtility
+from zope.app.intid.interfaces import IIntIds
+from z3c.relationfield.event import _relations, updateRelations
+
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.churches to 1005',
+                description=u'Upgrade wcc.churches to 1005',
+                source='1004', destination='1005',
+                sortkey=1, profile='wcc.churches:default')
+def to1005(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.churches.upgrades:to1005')
+
+    portal_catalog = getToolByName(context, 'portal_catalog')
+
+    catalog = getUtility(ICatalog)
+    intids = getUtility(IIntIds)
+
+    for b in portal_catalog(
+            portal_type=[
+                'wcc.churches.churchmember',
+                'wcc.churches.churchfamily',
+                'wcc.churches.churchbody'],
+            Language='all'):
+        obj = b.getObject()
+        for name, relation in _relations(obj):
+            try:
+                relation.to_id = intids.getId(relation.to_object)
+            except KeyError:
+                continue
+        updateRelations(obj, None)
 
 
 @gs.upgradestep(title=u'Upgrade wcc.churches to 1004',
@@ -53,3 +86,34 @@ def to1002(context):
             intids.register(obj)
         except (AttributeError, KeyError):
             pass
+
+#from five.intid.site import del_intids
+#from plone.app.intid.setuphandlers import add_intids, registerContent
+#from Products.CMFCore.interfaces import IDynamicType
+#from zope.component import getUtility
+#from zope.app.intid.interfaces import IIntIds
+#from z3c.relationfield.interfaces import IHasRelations
+#from zc.relation.interfaces import ICatalog
+#from zope.component.hooks import setSite
+#def rebuild(site):
+#    setSite(site)
+#    del_intids(site)
+#    add_intids(site)
+#    catalog = site.portal_catalog
+#    print "Rebuild catalog"
+#    catalog.clearFindAndRebuild()
+#    intids = getUtility(IIntIds)
+#    print "Rebuild intid"
+#    brains = catalog(object_provides=IDynamicType.__identifier__, Language='all')
+#    for brain in brains:
+#        intids.register(brain.getObject())
+#    print "Rebuild relations"
+#    rcatalog = getUtility(ICatalog)
+#    rcatalog.clear()
+#    brains = catalog.searchResults(
+#            object_provides=IHasRelations.__identifier__,
+#            Language='all')
+#    for brain in brains:
+#        obj = brain.getObject()
+#        updateRelations(obj, None)
+#
