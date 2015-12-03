c9e1767d871711c478c126406c8a58241feef859
glenn <glenn@glenn-Lenovo-G480>
2015-12-03 12:01:07 +0800

    added registry

--- wcc/churches/setuphandlers.py
+++ wcc/churches/setuphandlers.py
@@ -1,5 +1,8 @@
 from collective.grok import gs
 from wcc.churches import MessageFactory as _
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from wcc.churches.interfaces import IWccChurchSettings
 
 @gs.importstep(
     name=u'wcc.churches', 
@@ -9,5 +12,7 @@ def setupVarious(context):
     if context.readDataFile('wcc.churches.marker.txt') is None:
         return
     portal = context.getSite()
+    registry = getUtility(IRegistry)
+    registry.registerInterface(IWccChurchSettings)
 
     # do anything here
