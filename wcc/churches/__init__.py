291e7ccf9ae5bedecb731ead64222cec8e08ee43
glenn <glenn@glenn-Lenovo-G480>
2015-12-03 11:55:58 +0800

    register settings

--- wcc/churches/__init__.py
+++ wcc/churches/__init__.py
@@ -3,6 +3,9 @@ from Products.CMFQuickInstallerTool.interfaces import INonInstallable
 from five import grok
 from collective.grok import gs
 from zope.i18nmessageid import MessageFactory
+from plone.registry.interfaces import IRegistry
+from wcc.churches.interfaces import IWccChurchSettings
+from zope.component import getUtility
 
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('wcc.churches')
@@ -23,3 +26,12 @@ gs.profile(name=u'default',
            title=u'wcc.churches',
            description=_(u''),
            directory='profiles/default')
+
+def getSettings():
+    registry = getUtility(IRegistry)
+    try:
+        return registry.forInterface(IWccChurchSettings)
+    except:
+        registry.registerInterface(IWccChurchSettings)
+    return registry.forInterface(IWccChurchSettings)
+
